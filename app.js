8ccd6a8ce42d518410476010ef1a9e59f19cdf12
LucasMartinsAlves <lucas.martinslvs08@gmail.com>
2022-04-27 10:51:33 -0300

    Ajustes

--- app.js
+++ app.js
@@ -3,17 +3,39 @@
 const aboutUs = document.querySelector(".btn-about");
 const contactUs = document.querySelector(".btn-contact");
 
+const inicio = document.querySelector(".inicio");
+const sobre = document.querySelector(".sobre");
+const suporte = document.querySelector(".suporte");
+
+document.getElementById("radio1").checked = true;
+
+//counter
+
+let count = 1;
+setInterval(function () {
+  nextImage();
+}, 5000);
+
 //Event Listening
 
 aboutUs.addEventListener("click", moveAbout);
 contactUs.addEventListener("click", moveContact);
+inicio.addEventListener("click", moveInicio);
+sobre.addEventListener("click", moveContact);
+suporte.addEventListener("click", moveContact);
 
 //Functions
 
-function moveAbout() {
-  window.scrollTo(0, 948);
+function moveInicio() {
+  window.scrollTo(0, 0);
 }
 
-function moveContact() {
-  window.scrollTo(0, 1452);
+function nextImage() {
+  count++;
+
+  if (count > 4) {
+    count = 1;
+  }
+
+  document.getElementById("radio" + count).checked = true;
 }
